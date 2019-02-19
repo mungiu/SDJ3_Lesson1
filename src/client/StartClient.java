6fabd6bec11b3891915eee54bdfc83d9f524143a
Andrei Mungiu <andrei_mungiu@yahoo.com>
2019-02-19 17:41:16 +0100

    cosmetic

--- src/client/StartClient.java
+++ src/client/StartClient.java
@@ -10,20 +10,20 @@ import SharedInterfaces.IClientProxy;
 
 public class StartClient {
     public static void main(String[] args) throws RemoteException, MalformedURLException, NotBoundException {
-	String userInput;
-	IClientProxy iClientProxy = new ClientController();
-	Naming.rebind("iClientProxy", iClientProxy);
-	((ClientController) iClientProxy).registerClientOnServer();
-	
-	System.out.println("Client started...");
+        String userInput;
+        IClientProxy iClientProxy = new ClientController();
+        Naming.rebind("iClientProxy", iClientProxy);
+        ((ClientController) iClientProxy).registerClientOnServer();
 
-	Scanner keyboard = new Scanner(System.in);
+        System.out.println("Client started...");
 
-	while (true) {
-	    userInput = keyboard.nextLine();
-	    keyboard.close();
-	    System.out.println(keyboard.nextLine());
-	    ((ClientController) iClientProxy).clientInput(userInput);
-	}
+        Scanner keyboard = new Scanner(System.in);
+
+        while (true) {
+            userInput = keyboard.nextLine();
+            keyboard.close();
+            System.out.println(keyboard.nextLine());
+            ((ClientController) iClientProxy).clientInput(userInput);
+        }
     }
 }
